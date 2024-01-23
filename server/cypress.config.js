a1b4e177a86e781496e0c99144128a443a255a3c
jpronkko <jukka.ronkko@iki.fi>
2024-01-23 13:59:51 +0200

    Cypress render resolution change

--- server/cypress.config.js
+++ server/cypress.config.js
@@ -4,8 +4,8 @@ module.exports = defineConfig({
   projectId: 'frryer',
   e2e: {
     defaultCommandTimeout: 60000,
-    viewportWidth: 1200,
-    viewportHeight: 768,
+    viewportWidth: 1600,
+    viewportHeight: 1024,
     options: {
       browser: 'chrome',
     },
